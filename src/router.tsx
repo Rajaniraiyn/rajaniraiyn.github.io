a71ec48576d0afe2977c25440b9ef3636ac30384
Rajaniraiyn R <rajaniraiyn@gmail.com>
2025-11-05 02:00:59 +0530

    reduce rerenders

--- src/router.tsx
+++ src/router.tsx
@@ -15,6 +15,7 @@ export function getRouter() {
         routeTree,
         defaultPreload: 'intent',
         scrollRestoration: true,
+        defaultStructuralSharing: true,
         Wrap: ({ children }) => (
             <ThemeProvider>
                 <TooltipProvider>
